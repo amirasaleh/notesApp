17c244473d507889ff7c8bd953fa6383d6cef540
amirasaleh <83368692+amirasaleh@users.noreply.github.com>
2024-10-27 14:37:16 +0300

    add field to note bottom sheet

--- lib/views/widgets/custom_text_field.dart
+++ lib/views/widgets/custom_text_field.dart
@@ -2,28 +2,28 @@ import 'package:flutter/material.dart';
 import 'package:notesapp/constants.dart';
 
 class CustomTextField extends StatelessWidget {
-  const CustomTextField({super.key});
+  const CustomTextField({
+    super.key,
+    required this.hint,  this.maxLines=1,
+  });
+  final String hint;
+  final int maxLines;
 
   @override
   Widget build(BuildContext context) {
-    return  TextField(
+    return TextField(
       cursorColor: kPrimayColor,
       decoration: InputDecoration(
-        hintText: 'Title',
-        hintStyle:const TextStyle(
-          color: kPrimayColor
-        ),
-        border: buildBorder(),
-        enabledBorder: buildBorder(),
-        focusedBorder: buildBorder(kPrimayColor)
-      ),
+          hintText: hint,
+          border: buildBorder(),
+          enabledBorder: buildBorder(),
+          focusedBorder: buildBorder(kPrimayColor)),
     );
   }
 
   OutlineInputBorder buildBorder([color]) {
     return OutlineInputBorder(
         borderRadius: BorderRadius.circular(16),
-        borderSide:  BorderSide(color: color ??Colors.white)
-      );
+        borderSide: BorderSide(color: color ?? Colors.white));
   }
 }
--- lib/views/widgets/note_add_buttom_sheet.dart
+++ lib/views/widgets/note_add_buttom_sheet.dart
@@ -1,20 +1,32 @@
 
+import 'dart:math';
+
 import 'package:flutter/material.dart';
-import 'package:notesapp/views/widgets/text_field.dart';
+import 'package:notesapp/views/widgets/custom_text_field.dart';
 
 class NoteAddButtomSheet extends StatelessWidget {
   const NoteAddButtomSheet({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 16),
+    return const Padding(
+      padding:  EdgeInsets.symmetric(horizontal: 16),
       child: Column(
-        children: [
+        children:[
           SizedBox(
             height: 32,
           ),
-          CustomTextField()
+          CustomTextField(
+            hint: 'title',
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          CustomTextField(
+            hint: 'content',
+            maxLines: 5,
+          )
+
         ],
       ),
     );
